dfa5e26ea7020c21f36f04a97c8e795dc2ec38d1
HrishikThakur1 <rishikyo7@gmail.com>
2025-08-13 17:11:47 +0530

    Serve frontend from backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,20 +4,31 @@ import fetch from "node-fetch";
 import cors from "cors";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
 app.use("/api", chatRoutes);
+
+
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
+});
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected with Database!");
 
-        
         app.listen(PORT, () => {
             console.log(`server running on ${PORT}`);
         });
@@ -28,5 +39,4 @@ const connectDB = async () => {
 };
 
 connectDB();
-
 
